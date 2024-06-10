56fa611a1c9bcb628a0c153ebafc38b5e2842a28
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-06-10 18:24:49 +0530

    done

--- src/Components/ProjectSlider.jsx
+++ src/Components/ProjectSlider.jsx
@@ -11,39 +11,52 @@ const ProjectSlider = ({ projects }) => {
         infinite: true,
         speed: 500,
         slidesToShow: 2,
-        slidesToScroll: 2
-      };
+        slidesToScroll: 2,
+    };
+
     return (
         <>
             <div className="w-full overflow-hidden">
-            <Slider {...settings}>
-                {projects.map((project, index) => (
-                    <Link to={project.link} target="#" key={index}>
-                    <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-                            <motion.img
-                            whileInView={{opacity:1,y:0}}
-                            initial={{opacity:0,y:-100}}
-                            transition={{duration:1}}
-                            src={project.image} alt={project.title} className="mb-6 rounded" />
-                        <motion.div
-                        whileInView={{opacity:1,y:0}}
-                        initial={{opacity:0,y:-100}}
-                        transition={{duration:1}}
-                        className="w-full max-w-xl lg:w-3/4">
-                            <h6 className="mb-2 font-semibold">{project.title}</h6>
-                            <h6 className="mb-4 text-neutral-400">{project.description}</h6>
-                            {project.technologies.map((tech,index)=>(
-                                <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">
-                                    {tech}
-                                </span>
-                            ))}
-                        </motion.div>
-                    </div>
-                    </Link>
-                ))}
-            </Slider>
-        </div>
+                <Slider {...settings}>
+                    {projects.map((project, index) => (
+                        <Link to={project.link} target="#" key={index}>
+                            <div
+                                key={index}
+                                className="mb-8 flex flex-wrap lg:justify-center"
+                            >
+                                <motion.img
+                                    whileInView={{ opacity: 1, y: 0 }}
+                                    initial={{ opacity: 0, y: -100 }}
+                                    transition={{ duration: 1 }}
+                                    src={project.image}
+                                    alt={project.title}
+                                    className="mb-6 rounded w-[35rem] h-[30rem] object-cover"
+                                />
+                                <motion.div
+                                    whileInView={{ opacity: 1, y: 0 }}
+                                    initial={{ opacity: 0, y: -100 }}
+                                    transition={{ duration: 1 }}
+                                    className="w-full max-w-xl lg:w-3/4"
+                                >
+                                    <h6 className="mb-2 font-semibold">{project.title}</h6>
+                                    <h6 className="mb-4 text-neutral-400">
+                                        {project.description}
+                                    </h6>
+                                    {project.technologies.map((tech, index) => (
+                                        <span
+                                            key={index}
+                                            className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
+                                        >
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </motion.div>
+                            </div>
+                        </Link>
+                    ))}
+                </Slider>
+            </div>
         </>
-    )
-}
-export default ProjectSlider
+    );
+};
+export default ProjectSlider;
